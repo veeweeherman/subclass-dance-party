3e6f057de2f14a25ce91ffa3ad916e8346c0f5b3
TGA-Student <tga.student@hackreactor.com>
2015-07-08 20:52:52 -0700

    added img and gif to dancer elements

--- src/tinyDancer.js
+++ src/tinyDancer.js
@@ -3,7 +3,8 @@ var makeTinyDancer = function(top, left, timeBetweenSteps){
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   // this.oldStep = this.step;
-  this.$node = $('<span class="dancer tiny"></span>');
+  // this.$node = $('<span class="dancer tiny"></span>');
+  this.$node = $('<span class="dancer tiny"><img src="http://i.imgur.com/vDNZM1D.gif"></span>');
   this.setPosition(top, left);
 };
 
